2ad26a73fd39a0d0a7d040e09f2ade6d001e5779
John Behan <behanj@gmail.com>
2016-04-04 20:01:13 +0700

    filtering setup

--- src/BugFilter.js
+++ src/BugFilter.js
@@ -1,13 +1,38 @@
 var React = require('react');
 
 var BugFilter = React.createClass({
+
+  getInitialState: function() {
+    return {priority: '', status: ''};
+  },
+
+  handlePriorityChange: function(event) {
+    this.setState({priority: event.target.value});
+  },
+
+  handleStatusChange: function(event) {
+    this.setState({status: event.target.value});
+  },
+
   handleClick: function() {
-    this.props.handleSubmit({status: 'open'});
+    this.props.handleSubmit({status: this.state.status, priority: this.state.priority});
   },
 
   render: function() {
     return (
       <div className="bugFilter">
+        <select name="priority" value={this.state.priority} onChange={this.handlePriorityChange}>
+          <option value="">Filter by Priority...</option>
+          <option value="manyana">Manyana</option>
+          <option value="urgent">Urgent</option>
+        </select>
+        <select name="status" value={this.state.status} onChange={this.handleStatusChange}>
+          <option value="">Filter by Status...</option>
+          <option value="closed">Closed</option>
+          <option value="open">Open</option>
+          <option value="pending">Pending</option>
+          <option value="uclosed">UClosed?</option>
+        </select>
         <button onClick={this.handleClick}>Filter</button>
       </div>
     );
